0b724147e1dde30bca6634b73305149c2f25ce8a
Ryan Morales <ryan.morales@west-mec.org>
2025-01-10 20:15:24 +0000

    started bubble sort

---
+++ php_practice/algorithms/bubblesort.php
@@ -0,0 +1,10 @@
+<?php
+echo "hello";
+
+function bubbleSort(array $arr): array {
+    $n = count($arr);
+    echo "length of $arr is $n";
+    return $arr;
+}
+$arr = [1, 2, 3, 4, 5];
+bubbleSort($arr);
